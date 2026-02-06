0e97ebe8e273a82ac3ce9ddaa64c90112368c657
Kishan Khunt <kishankhunt.work@gmail.com>
2026-02-06 21:20:43 +0530

    first commit

---
+++ src/utils/constant.ts
@@ -0,0 +1,88 @@
+export const data = {
+  email: "elite.elecon04@gmail.com",
+  mobile: "+919898562624",
+  mobile2: "+918511114499",
+  address: "2, First Floor, Diamond Complex, Stand Chowk, Jetpur, 360370",
+  businessHours: "Mon-Sat: 8:00 AM - 6:00 PM",
+  socialMedia: {
+    facebook: "#",
+    twitter: "#",
+    instagram: "#",
+    linkedin: "#",
+    youtube: "#",
+  },
+};
+
+
+export const partners = [
+  {
+    id: 1,
+    name: "Blue Star Ltd.",
+    website: "https://www.bluestarindia.com",
+  },
+  {
+    id: 2,
+    name: "Shreeji Krupa Con.",
+    website: "#",
+  },
+  {
+    id: 3,
+    name: "Jay Hanuman Infra.",
+    website: "#",
+  },
+  {
+    id: 4,
+    name: "Godrej & Boyce Co. Ltd.",
+    website: "https://www.godrej.com",
+  },
+  {
+    id: 5,
+    name: "Patel Infrastructure",
+    website: "#",
+  },
+  {
+    id: 6,
+    name: "CMC Project PVT. Ltd.",
+    website: "#",
+  },
+  {
+    id: 7,
+    name: "BD Buildcon",
+    website: "#",
+  },
+  {
+    id: 8,
+    name: "Sai Fabrication",
+    website: "#",
+  },
+  {
+    id: 9,
+    name: "L & T Ltd.",
+    website: "https://www.larsentoubro.com",
+  },
+  {
+    id: 10,
+    name: "Dilip BuildCon Ltd.",
+    website: "#",
+  },
+  {
+    id: 11,
+    name: "KEC International Ltd.",
+    website: "https://www.kecrpg.com",
+  },
+  {
+    id: 12,
+    name: "Adani Green Energy",
+    website: "https://www.adanigreenenergy.com",
+  },
+  {
+    id: 13,
+    name: "Onix Renewable Ltd.",
+    website: "#",
+  },
+  {
+    id: 14,
+    name: "EHES Ltd.",
+    website: "#",
+  },
+];
